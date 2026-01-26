6c1e898560ce319dc18332897ce5b22528d1535f
BenzFarinas <farinasbenz@gmail.com>
2026-01-25 21:26:40 -0500

    add q1, q2, q5

---
+++ Week01/lab1.py
@@ -0,0 +1,13 @@
+# Sample Coding Question
+# Benz Farinas
+
+
+
+# Question 2: Defining an array (list)
+my_array = [1, 4, 7, 9]
+
+#Q3
+
+#Q4
+
+#Q5
---
+++ Week01/q5.py
@@ -0,0 +1,5 @@
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 22
+
+print("Now showing the shop items filtered by age:", userAge)
